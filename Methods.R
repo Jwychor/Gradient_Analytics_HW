652123048122a0ad44f1ebdc7b44a7deed10bda2
Jwychor <53948645+Jwychor@users.noreply.github.com>
2020-11-30 10:47:22 -0600

    Developing CLM Model

--- Methods.R
+++ Methods.R
@@ -1,2 +1,17 @@
 library(tidyverse)
 
+chi-sq.matrix <- function(data) {
+  # Check if 'data' argument is a dataframe
+  if(!is.data.frame(data)){
+    stop("Argument 'data' must be a dataframe")
+  }
+  
+  # Check if each column is a factor
+  map(data, function(column){
+    if(!is.factor(column) | is.character(column)){
+      stop("All columns must be type 'factor' or 'character'")
+    }
+  })
+  
+}
+
--- Workspace.R
+++ Workspace.R
@@ -11,19 +11,30 @@ setwd("C:\\Users\\jwych\\Documents\\GitHub\\Gradient_Analytics_HW")
 
 #### Q1 ####
 experiment_data <- read_sav('data/experiment_data.sav')
-
-experiment_data$price
+?melt
+colnames(experiment_data)
+levels(experiment_data$rtb)
 
 ORModel <- experiment_data %>%
-  mutate_all(~as.factor(.)) %>%
+  mutate_all(~ as.factor(.)) %>%
   polr(answer ~ duration + offer + outcome + price + rtb + social_proof,
        data = ., Hess = T)
+
+install.packages('data.table')
+
+experiment_data %>%
+  group_by(response_id) %>%
+  summarize(GroupVariance = var(rep(answer)))
 
 experiment_data %>%
   select_if(!str_detect(names(.),"response_id|task")) %>%
   gather() %>%
   count(.$value) %>%
   print(., n = 28)
+
+experiment_data %>%
+  mutate_all(~ as.character(.)) %>%
+  melt(id = c("response_id"))
 
 experiment_data %>%
   melt(id = c("response_id")) %>%
